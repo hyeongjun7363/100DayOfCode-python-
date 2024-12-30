f28bffaa02bd2923662d0c4d64d2bbfa94bedb9d
hyeongjun7363 <hyeongjun7363@gmail.com>
2024-12-30 17:16:49 +0900

    Day1

---
+++ Day1/Day1.py
@@ -0,0 +1,13 @@
+#task1 - print 기능구현
+print("Hello. world!")
+
+#task2 - print 기능 및 다음줄 넘어가는 기능
+print("Hello. world!\nHello. world!\nHello. world!")
+
+#task3 - input 기능을 다른 fuction안에 사용
+print("Hello " + input("waht is your name? ") + "!")
+
+#task4
+name = input("What is your name? ")
+print("Hello" + name)
+print(len(name))
